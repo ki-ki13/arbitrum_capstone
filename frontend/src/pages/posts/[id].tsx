a2d90fb8881982c7ccf992a35e686b3a972bc577
ki-ki13 <Rizkimahjati845@students.unnes.ac.id>
2024-09-28 23:43:56 +0700

    fix post folder

--- frontend/src/pages/posts/[id].tsx
+++ frontend/src/pages/posts/[id].tsx
@@ -1,17 +1,17 @@
 import { useRouter } from "next/router";
-import { deployedAddress } from "../../../../contracts/deployed-contract";
+import { deployedAddress } from "../../contracts/deployed-contract";
 import { useEffect, useState } from "react";
-import ShareablePostComponent from "../../../../components/ShareablePostComponent";
-import Comments from "../../../../components/Comments";
+import ShareablePostComponent from "../../components/ShareablePostComponent";
+import Comments from "../../components/Comments";
 import type { ParsedUrlQuery } from "node:querystring";
 import { getAccount } from "@wagmi/core";
-import config from "../../../../wagmi";
+import config from "../../wagmi";
 import Link from "next/link";
-import Poll from "../../../../components/Poll";
-import styles from "../../../../styles/Custom.module.css"; 
+import Poll from "../../components/Poll";
+import styles from "../../styles/Custom.module.css"; 
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { useReadForumGetPost } from "../../../../contracts/generated";
-import CommentForm from "../../../../components/CommentForm";
+import { useReadForumGetPost } from "../../contracts/generated";
+import CommentForm from "../../components/CommentForm";
 
 export interface PostIdParams extends ParsedUrlQuery {
 	id: string;
